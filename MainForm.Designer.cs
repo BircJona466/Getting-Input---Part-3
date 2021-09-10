665d71a728976178deb658fc621d54c206f086a0
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-09-10 13:13:13 -0400

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Getting_Input___Part_3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.SkyBlue;
+            this.btnSubmit.Location = new System.Drawing.Point(44, 51);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(115, 29);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Click Me!";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(165, 25);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(115, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(41, 28);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(118, 13);
+            this.lblInstruction.TabIndex = 2;
+            this.lblInstruction.Text = "Please enter your name";
+            this.lblInstruction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblInstruction.Click += new System.EventHandler(this.lblInstruction_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(196, 60);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(50, 13);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "Message";
+            this.lblMessage.Click += new System.EventHandler(this.lblMessage_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 105);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Getting Input";
+            this.Click += new System.EventHandler(this.MainForm_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Getting_Input___Part_3
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lblInstruction_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMessage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name = txtInput.Text;
+            lblMessage.Text = "Hello " + name;
+        }
+    }
+}
